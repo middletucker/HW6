01bd79a82945aec19264fc19c910306a9ab4bcf3
middletucker <80505827+middletucker@users.noreply.github.com>
2022-10-03 01:40:37 +0000

    finish P4

--- P4/main4.cpp
+++ P4/main4.cpp
@@ -1,33 +1,34 @@
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 string Astrology(int monthNum, int day);
+int MonthtoNum(string month);
 
-int main()
-{
+int main() {
   string sign, month;
-  int day, numPeople;
+  int day, monthNum, numPeople;
 
   cin >> numPeople;
 
-  for (int i; i < numPeople; i++)
-    {
-      cin >> day >> month;
+  for (int i; i < numPeople; i++) {
+    cin >> day;
+    cin >> month;
 
-      
+    monthNum = MonthtoNum(month);
 
-      cout << sign << endl;
-    }
+    sign = Astrology(monthNum, day);
+
+    cout << sign << endl;
+  }
 
   return 0;
 }
 
-int MonthtoNum (string month)
-{
+int MonthtoNum(string month) {
   int monthNum;
 
   if (month == "Jan")
@@ -40,78 +41,105 @@ int MonthtoNum (string month)
     monthNum = 4;
   else if (month == "May")
     monthNum = 5;
-
+  else if (month == "Jun")
+    monthNum = 6;
+  else if (month == "Jul")
+    monthNum = 7;
+  else if (month == "Aug")
+    monthNum = 8;
+  else if (month == "Sep")
+    monthNum = 9;
+  else if (month == "Oct")
+    monthNum = 10;
+  else if (month == "Nov")
+    monthNum = 11;
+  else if (month == "Dec")
+    monthNum = 12;
+  else
+    cout << "Error in MonthtoNum\n";
 
   return monthNum;
 }
 
-string Astrology(int monthNum, int day)
-{
+string Astrology(int monthNum, int day) {
   string sign;
 
-  switch(monthNum)
-    {
-      case 1:
-      if (day <= 20)
-        sign = "Capricorn\n";
-      else if (day > 20)
-        sign = "Aquarius\n";
-      case 2:
-      if (day <= 19)
-        sign = "Aquarius\n";
-      else if (day > 19)
-        sign = "Pisces\n";
-      case 3:
-      if (day <= 20)
-        sign = "Pisces\n";
-      else if (day > 20)
-        sign = "Aries\n";
-      case 4:
-      if (day <= 20)
-        sign = "Aries\n";
-      else if (day > 20)
-        sign = "Taurus\n";
-      case 5:
-      if (day <= 20)
-        sign = "Taurus\n";
-      else if (day > 20)
-        sign = "Gemini\n";
-      case 6:
-      if (day <= 21)
-        sign = "Gemini\n";
-      else if (day > 21)
-        sign = "Cancer\n";
-      case 7:
-      if (day <= 22)
-        sign = "Cancer\n";
-      else if (day > 22)
-        sign = "Leo\n";
-      case 8:
-      if (day <= 22)
-        sign = "Leo\n";
-      else if (day > 22)
-        sign = "Virgo\n";
-      case 9:
-      if (day <= 21)
-        sign = "Virgo\n";
-      else if (day > 21)
-        sign = "Libra\n";
-      case 10:
-      if (day <= 22)
-        sign = "Libra\n";
-      else if (day > 22)
-        sign = "Scorpio\n";
-      case 11:
-      if (day <= 22)
-        sign = "Scorpio\n";
-      else if (day > 22)
-        sign = "Sagittarius\n";
-      case 12:
-      if (day <= 21)
-        sign = "Sagittarius\n";
-      else if (day > 21)
-        sign = "Capricorn\n";
-    }
-    
+  switch (monthNum) {
+  case 1:
+    if (day <= 20)
+      sign = "Capricorn\n";
+    else if (day > 20)
+      sign = "Aquarius\n";
+    break;
+  case 2:
+    if (day <= 19)
+      sign = "Aquarius\n";
+    else if (day > 19)
+      sign = "Pisces\n";
+    break;
+  case 3:
+    if (day <= 20)
+      sign = "Pisces\n";
+    else if (day > 20)
+      sign = "Aries\n";
+    break;
+  case 4:
+    if (day <= 20)
+      sign = "Aries\n";
+    else if (day > 20)
+      sign = "Taurus\n";
+    break;
+  case 5:
+    if (day <= 20)
+      sign = "Taurus\n";
+    else if (day > 20)
+      sign = "Gemini\n";
+    break;
+  case 6:
+    if (day <= 21)
+      sign = "Gemini\n";
+    else if (day > 21)
+      sign = "Cancer\n";
+    break;
+  case 7:
+    if (day <= 22)
+      sign = "Cancer\n";
+    else if (day > 22)
+      sign = "Leo\n";
+    break;
+  case 8:
+    if (day <= 22)
+      sign = "Leo\n";
+    else if (day > 22)
+      sign = "Virgo\n";
+    break;
+  case 9:
+    if (day <= 21)
+      sign = "Virgo\n";
+    else if (day > 21)
+      sign = "Libra\n";
+    break;
+  case 10:
+    if (day <= 22)
+      sign = "Libra\n";
+    else if (day > 22)
+      sign = "Scorpio\n";
+    break;
+  case 11:
+    if (day <= 22)
+      sign = "Scorpio\n";
+    else if (day > 22)
+      sign = "Sagittarius\n";
+    break;
+  case 12:
+    if (day <= 21)
+      sign = "Sagittarius\n";
+    else if (day > 21)
+      sign = "Capricorn\n";
+    break;
+  default:
+    cout << "Error in Astrology\n";
+  }
+
   return sign;
 }
